04018954995ce2b3f3f8e4dfab96910e7446d548
StepSenpai <StepSenpai1998@gmail.com>
2023-09-21 20:56:52 +0200

    commitSOLVED

--- src/main/java/Ventana.java
+++ src/main/java/Ventana.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.util.Objects;
+
 public class Ventana extends JFrame {
     private JPanel mainPanel;
     private JButton neutralWhiteButton;
@@ -8,11 +10,11 @@ public class Ventana extends JFrame {
     private JLabel labelTalk;
     private JPanel buttonPanel;
     private JPanel imgPanel;
-
-    //Situacion que cambia cuando el usuario interactua
-    private static int situation = 0;
-    //Texto que va a ser mostrado
-    private static String kokoTalk;
+    private int i = -1;
+    private String kokoTalk;
+    private final Timer timer = new Timer(0, e -> {
+        if (i++ < kokoTalk.length() - 1) labelTalk.setText(labelTalk.getText() + kokoTalk.charAt(i));
+    });
 
     /**
      * Constructor e initiator
@@ -23,95 +25,53 @@ public class Ventana extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("KOKOTalk BETA");
         setVisible(true);
-        startAll();
+        startAll(0);
 
-        neutralWhiteButton.addActionListener(e -> {
-            situation = 0;
-            startAll();
-        });
-        redSadButton.addActionListener(e -> {
-            situation = 1;
-            startAll();
-        });
-        greenHappyButton.addActionListener(e -> {
-            situation = 2;
-            startAll();
-        });
+        neutralWhiteButton.addActionListener(e -> startAll(0));
+        redSadButton.addActionListener(e -> startAll(1));
+        greenHappyButton.addActionListener(e -> startAll(2));
     }
 
     /**
-     * Starts all methods
+     * Reinicia el estado del timer y empieza los metodos de seleccion de imagen y texto
      */
-    private void startAll() {
-        loadPics();
-        selectText();
-        timerTalk();
+    private void startAll(int situation) {
+        i = -1;
+        labelTalk.setText("<html>");
+        loadPics(situation);
+        kokoTalk = selectText(situation);
+        timer.restart();
     }
 
     /**
      * Selecciona la imagen que va a mostrar
      */
-    private void loadPics() {
-        if (situation < 0 || situation > 2)
-            situation = 0;
+    private void loadPics(int situation) {
         switch (situation) {
-            case 0:
-                labelPic.setIcon(new ImageIcon(getClass().getResource("/img/kokoStickerRegular.png")));
-                break;
-            case 1:
-                labelPic.setIcon(new ImageIcon(getClass().getResource("/img/kokoStickerFail.png")));
-                break;
-            case 2:
-                labelPic.setIcon(new ImageIcon(getClass().getResource("/img/kokoStickerSuccess.png")));
-                break;
+            default ->
+                    labelPic.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/img/kokoStickerRegular.png"))));
+            case 1 ->
+                    labelPic.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/img/kokoStickerFail.png"))));
+            case 2 ->
+                    labelPic.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/img/kokoStickerSuccess.png"))));
         }
     }
 
     /**
      * Selecciona el texto que va a mostrar
      */
-    private void selectText() {
-        kokoTalk = "";
-        if (situation < 0 || situation > 2)
-            situation = 0;
-        switch (situation) {
-            case 0:
-                kokoTalk = "<html> I am Sangonomiya Kokomi, the Divine Priestess of Watatsumi Island. My journey with you" +
-                        " will be an opportunity to unwind... Ahem, I mean... to survey beyond our borders. Don't worry, " +
-                        "I've left behind ample directives in my absence, and have also completed the island's projected " +
-                        "development plans. Departing for a brief period will be of little consequence.\n<html>";
-                break;
-            case 1:
-                kokoTalk = "<html><p><span style=\"color: red;\">No matter how pressing the situation may become, I must " +
-                        "remain composed. If I let the stress get to me, then what can I expect from others around me?<p><span><html>";
-                break;
-            case 2:
-                kokoTalk = "<html><p><span style=\"color: green;\">Huh? What do I enjoy? Simple. I like to find a quiet " +
-                        "place by myself and read books on strategy... Ahh, the whole day without interruption. I'd be " +
-                        "even happier if we looked over some of it together... One thing though... You'd have to sleep " +
-                        "on the floor.<p><span><html>";
-                break;
-        }
-    }
-
-    //TODO
-    // Fallo consiste en que el Timer una vez creado no se cierra y continua realizandose, no solo ralentizando
-    // el proceso pero tambien continuando a meter letras que estropean el textp
-
-    /**
-     * Timer, se encarga de mostrar caracter por caracter el texto
-     */
-    private void timerTalk() {
-        labelTalk.setText("");
-        int[] index = {0};
-        char[] kokoTalkChar = kokoTalk.toCharArray();
-        Timer timer = new Timer(0, e -> {
-            if (index[0] < kokoTalkChar.length) {
-                labelTalk.setText(labelTalk.getText() + kokoTalkChar[index[0]]);
-                index[0]++;
-            }
-        });
-        timer.setRepeats(true);
-        timer.start();
+    private String selectText(int situation) {
+        return switch (situation) {
+            default -> " I am Sangonomiya Kokomi, the Divine Priestess of Watatsumi Island. My journey with you" +
+                    " will be an opportunity to unwind... Ahem, I mean... to survey beyond our borders. Don't worry, " +
+                    "I've left behind ample directives in my absence, and have also completed the island's projected " +
+                    "development plans. Departing for a brief period will be of little consequence.\n<html>";
+            case 1 -> "<p><span style=\"color: red;\">No matter how pressing the situation may become, I must " +
+                    "remain composed. If I let the stress get to me, then what can I expect from others around me?<p><span><html>";
+            case 2 -> "<p><span style=\"color: green;\">Huh? What do I enjoy? Simple. I like to find a quiet " +
+                    "place by myself and read books on strategy... Ahh, the whole day without interruption. I'd be " +
+                    "even happier if we looked over some of it together... One thing though... You'd have to sleep " +
+                    "on the floor.<p><span><html>";
+        };
     }
 }
