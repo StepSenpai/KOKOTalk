cfaaaec64cf06f97579dff39381c6ece3e3b4255
StepSenpai <StepSenpai1998@gmail.com>
2023-09-22 20:47:24 +0200

    commitIMPROVED

--- src/main/java/Ventana.java
+++ src/main/java/Ventana.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.util.Objects;
 
 public class Ventana extends JFrame {
@@ -10,68 +11,68 @@ public class Ventana extends JFrame {
     private JLabel labelTalk;
     private JPanel buttonPanel;
     private JPanel imgPanel;
-    private int i = -1;
+    private int i;
     private String kokoTalk;
-    private final Timer timer = new Timer(0, e -> {
-        if (i++ < kokoTalk.length() - 1) labelTalk.setText(labelTalk.getText() + kokoTalk.charAt(i));
-    });
+    private final Timer timer = new Timer(0, this::timerAction);
+    private int situation;
 
-    /**
-     * Constructor e initiator
-     */
     public Ventana() {
+        startUp();
+        situationControl(0);
+        neutralWhiteButton.addActionListener(e -> situationControl(0));
+        redSadButton.addActionListener(e -> situationControl(1));
+        greenHappyButton.addActionListener(e -> situationControl(2));
+    }
+
+    private void startUp() {
         setContentPane(mainPanel);
         setBounds(400, 200, 1000, 700);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("KOKOTalk BETA");
         setVisible(true);
-        startAll(0);
-
-        neutralWhiteButton.addActionListener(e -> startAll(0));
-        redSadButton.addActionListener(e -> startAll(1));
-        greenHappyButton.addActionListener(e -> startAll(2));
     }
 
-    /**
-     * Reinicia el estado del timer y empieza los metodos de seleccion de imagen y texto
-     */
-    private void startAll(int situation) {
-        i = -1;
-        labelTalk.setText("<html>");
-        loadPics(situation);
-        kokoTalk = selectText(situation);
+    private void situationControl(int situation) {
+        this.situation = situation;
+        reloadTextHead();
+        kokoTalk = selectText();
+        loadPics();
         timer.restart();
     }
 
-    /**
-     * Selecciona la imagen que va a mostrar
-     */
-    private void loadPics(int situation) {
-        switch (situation) {
-            default ->
-                    labelPic.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/img/kokoStickerRegular.png"))));
-            case 1 ->
-                    labelPic.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/img/kokoStickerFail.png"))));
-            case 2 ->
-                    labelPic.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/img/kokoStickerSuccess.png"))));
-        }
+    private void reloadTextHead() {
+        i = -1;
+        labelTalk.setText(switch (situation) {
+            default -> "<html>";
+            case 1 -> "<html><p><span style=\"color: red;\">";
+            case 2 -> "<html><p><span style=\"color: green;\">";
+        });
     }
 
-    /**
-     * Selecciona el texto que va a mostrar
-     */
-    private String selectText(int situation) {
+    private String selectText() {
         return switch (situation) {
             default -> " I am Sangonomiya Kokomi, the Divine Priestess of Watatsumi Island. My journey with you" +
                     " will be an opportunity to unwind... Ahem, I mean... to survey beyond our borders. Don't worry, " +
                     "I've left behind ample directives in my absence, and have also completed the island's projected " +
                     "development plans. Departing for a brief period will be of little consequence.\n<html>";
-            case 1 -> "<p><span style=\"color: red;\">No matter how pressing the situation may become, I must " +
+            case 1 -> " No matter how pressing the situation may become, I must " +
                     "remain composed. If I let the stress get to me, then what can I expect from others around me?<p><span><html>";
-            case 2 -> "<p><span style=\"color: green;\">Huh? What do I enjoy? Simple. I like to find a quiet " +
+            case 2 -> " Huh? What do I enjoy? Simple. I like to find a quiet " +
                     "place by myself and read books on strategy... Ahh, the whole day without interruption. I'd be " +
                     "even happier if we looked over some of it together... One thing though... You'd have to sleep " +
                     "on the floor.<p><span><html>";
         };
+    }
+
+    private void loadPics() {
+        labelPic.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource(switch (situation) {
+            default -> "/img/kokoStickerRegular.png";
+            case 1 -> "/img/kokoStickerFail.png";
+            case 2 -> "/img/kokoStickerSuccess.png";
+        }))));
+    }
+
+    private void timerAction(ActionEvent e) {
+        if (i++ < kokoTalk.length() - 1) labelTalk.setText(labelTalk.getText() + kokoTalk.charAt(i));
     }
 }
